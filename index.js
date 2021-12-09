34fbc4e0283077b09f154da05878cb1235ea7d1a
Wael A <waelvilla@gmail.com>
2021-12-09 13:48:09 +0300

    added server setup

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require('dotenv');
+const { createProxyMiddleware } = require("http-proxy-middleware");
+dotenv.config()
+// Create Express Server
+const app = express();
+
+// Configuration
+const {PORT, HOST, API_SERVICE_URL} = process.env;
+// Logging
+app.use(morgan("dev"));
+
+// Info GET endpoint
+app.get("/info", (req, res, next) => {
+  res.send(
+    "This is a proxy service which proxies to Billing and Account APIs."
+  );
+});
+
+// // Authorization
+// app.use("", (req, res, next) => {
+//   if (req.headers.authorization) {
+//     next();
+//   } else {
+//     res.sendStatus(403);
+//   }
+// });
+
+// Proxy endpoints
+app.use(
+  "/*",
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/*`]: "",
+    },
+  })
+);
+
+// Start the Proxy
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
